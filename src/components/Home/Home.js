f19ead1724f8eb1a00dc1a40d11fe43205bff39c
Finn <mailtorufina@gmail.com>
2023-08-13 22:08:11 +0100

    feat: render the hero images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import styles from "./Home.module.css";
 import Nav from "../Nav/Nav";
+import Hero from "../Hero/Hero";
 
 const Home = () => {
   return (
     <div>
       <h1 className={styles.header}>DURO</h1>
       <Nav />
+      <Hero />
     </div>
   );
 };

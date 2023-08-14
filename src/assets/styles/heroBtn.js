7d779f2c2a1aa18e9afe30ed00a302c191cfb466
Finn <mailtorufina@gmail.com>
2023-08-14 21:04:38 +0100

    styles: add hero button style file

---
+++ src/assets/styles/heroBtn.js
@@ -0,0 +1,10 @@
+const heroBtn = {
+  width: "20rem",
+  padding: "8px",
+  textTransform: "capitalize",
+  fontSize: "1.1rem",
+  fontWeight: "600",
+  borderRadius: "10px",
+};
+
+export default heroBtn;

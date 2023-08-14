6a80504583e80f960a820d0f9b269e6c0f7435bd
Finn <mailtorufina@gmail.com>
2023-08-14 21:13:34 +0100

    styles: add styling for the hero images

--- src/components/HeroImages/HeroImages.js
+++ src/components/HeroImages/HeroImages.js
@@ -1,11 +1,29 @@
 import styles from "./HeroImages.module.css";
+import { Button, ThemeProvider } from "@mui/material";
+import buttonTheme from "../../assets/themes/buttonThemes";
+import heroBtn from "../../assets/styles/heroBtn";
 
 const HeroImages = ({ src, title, description, reviews, ratings }) => {
   return (
-    <article>
-      <img src={src} alt={`"${src}"`} />
-      <h1 className={styles.header}> {title}</h1>
-      <p className={styles.description}>{description}</p>
+    <article className={styles.imageCard}>
+      <div className={styles.imageArea}>
+        <img src={src} alt={`"${src}"`} className={styles.image} />
+      </div>
+      <div className={styles.textArea}>
+        <h1 className={styles.header}>{title}</h1>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.innerBox}>
+          <ThemeProvider theme={buttonTheme}>
+            <Button variant="contained" sx={heroBtn}>
+              Get it now
+            </Button>
+          </ThemeProvider>
+          <div className={styles.feedbackBox}>
+            <p className={styles.reviews}>{reviews}</p>
+            <p className={styles.ratings}>{ratings}</p>
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
